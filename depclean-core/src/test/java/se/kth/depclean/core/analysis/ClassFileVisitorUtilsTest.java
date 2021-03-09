24f8e356bfea7bf732b0c0a25a3f8350e96d2b29
cesarsotovalero <cesarsotovalero@gmail.com>
2021-03-09 11:19:36 +0100

    Add trivial test just to get jacoco.xml report

---
+++ depclean-core/src/test/java/se/kth/depclean/core/analysis/ClassFileVisitorUtilsTest.java
@@ -0,0 +1,22 @@
+package se.kth.depclean.core.analysis;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClassFileVisitorUtilsTest {
+
+   @Before
+   public void setUp() throws Exception {
+   }
+
+   @After
+   public void tearDown() throws Exception {
+   }
+
+   @Test
+   public void accept() {
+      Assert.assertTrue(true);
+   }
+}
