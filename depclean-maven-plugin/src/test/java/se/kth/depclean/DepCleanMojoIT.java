6f4a3d47f2fbe961c06880a692312af457cbf146
cesarsotovalero <cesarsotovalero@gmail.com>
2021-03-22 15:11:20 +0100

    Organize IT

--- depclean-maven-plugin/src/test/java/se/kth/depclean/DepCleanMojoIT.java
+++ depclean-maven-plugin/src/test/java/se/kth/depclean/DepCleanMojoIT.java
@@ -1,21 +1,26 @@
 package se.kth.depclean;
 
+import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
+
 import com.soebes.itf.jupiter.extension.MavenGoal;
 import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
 import com.soebes.itf.jupiter.extension.MavenTest;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 
+/**
+ * This class executes integration tests against the DepCleanMojo.
+ */
 @MavenJupiterExtension
 public class DepCleanMojoIT {
 
   @MavenTest
   @MavenGoal("package")
-//  @EnabledForMavenVersion(MavenVersion.M3_6_3)
+  @DisplayName("Test that DepClean runs in empty Maven project")
   void first_very_simple(MavenExecutionResult result) {
-    Assertions.assertTrue(result.isSuccesful());
-//    assertThat(result).isSuccessful();
+    assertThat(result).isSuccessful();
   }
+
 
 }
 
