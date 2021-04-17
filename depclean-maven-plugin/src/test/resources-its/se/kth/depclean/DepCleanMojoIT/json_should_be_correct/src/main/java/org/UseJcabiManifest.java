93fceaa6c03fe61fd1201163153575cd59863e14
cesarsotovalero <cesarsotovalero@gmail.com>
2021-04-17 14:23:36 +0200

    Add missing project necessary for IT

---
+++ depclean-maven-plugin/src/test/resources-its/se/kth/depclean/DepCleanMojoIT/json_should_be_correct/src/main/java/org/UseJcabiManifest.java
@@ -0,0 +1,11 @@
+package org;
+
+import com.jcabi.manifests.Manifests;
+
+public class UseJcabiManifest {
+
+  public static void main(String[] args) {
+    Manifests manifests = new Manifests();
+    System.out.println(manifests.size());
+  }
+}
