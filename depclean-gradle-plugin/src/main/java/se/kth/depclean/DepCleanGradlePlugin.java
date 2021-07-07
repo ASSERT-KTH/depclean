220679084a93e291906ea4e614910cc1b89ae799
ABHAY SINGH <75938293+ABHAY0O7@users.noreply.github.com>
2021-07-08 05:07:24 +0530

    Added package-info files and refactored DepCleanGradlePlugin.class

--- depclean-gradle-plugin/src/main/java/se/kth/depclean/DepCleanGradlePlugin.java
+++ depclean-gradle-plugin/src/main/java/se/kth/depclean/DepCleanGradlePlugin.java
@@ -4,15 +4,23 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This plugin checks the bytecode of a gradle project and configure out whether a dependency
+ * is used in that project or not. After analysis, it also configures out the source of that
+ * dependency i.e. whether <br>
+ * <b>1)</b> It is declared directly in your build.gradle file or, <br>
+ * <b>2)</b> Inherited from any direct dependency or, <br>
+ * <b>3)</b> Just a transitive dependency.
+ */
 public class DepCleanGradlePlugin implements Plugin<Project> {
-
-  String taskName = "debloat";
 
   @Override
   public void apply(@NotNull Project project) {
 
+    final String depCleanConfigurationName = "depclean";
+
     // Creating extra configurations for the plugin to provide more flexibility.
-    project.getExtensions().create("depclean", DepCleanGradlePluginExtension.class);
+    project.getExtensions().create("depCleanConfigurationName", DepCleanGradlePluginExtension.class);
 
     // Creating the default task.
     createTask(project);
@@ -23,8 +31,9 @@ public class DepCleanGradlePlugin implements Plugin<Project> {
    *
    * @param project The Gradle project.
    */
-  public void createTask(Project project) {
-    DepCleanGradleTask task = project.getTasks().create(taskName, DepCleanGradleTask.class);
+  public void createTask(final Project project) {
+    final String depCleanTaskName = "debloat";
+    DepCleanGradleTask task = project.getTasks().create(depCleanTaskName, DepCleanGradleTask.class);
     task.setGroup("dependencyManagement");
     task.setDescription("Analyze the project byte-code and configure out the debloated dependencies.");
   }
---
+++ depclean-gradle-plugin/src/main/java/se/kth/depclean/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package to create the depClean Gradle Plugin.
+ */
+package se.kth.depclean;
