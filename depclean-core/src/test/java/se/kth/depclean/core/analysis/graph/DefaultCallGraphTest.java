6a9b7ecf0f0da297986b8e26c86d1d5abbc9e890
commandini <commandini@users.noreply.github.com>
2025-06-06 23:39:19 +0200

    chore: code clean-up

--- depclean-core/src/test/java/se/kth/depclean/core/analysis/graph/DefaultCallGraphTest.java
+++ depclean-core/src/test/java/se/kth/depclean/core/analysis/graph/DefaultCallGraphTest.java
@@ -1,49 +1,47 @@
 package se.kth.depclean.core.analysis.graph;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 @Slf4j
 class DefaultCallGraphTest {
 
   @BeforeEach
   void setUp() {
-    DefaultCallGraph.addEdge("A", new HashSet<>(Arrays.asList("B", "C", "D")));
-    DefaultCallGraph.addEdge("D", new HashSet<>(Arrays.asList("E", "F")));
-    DefaultCallGraph.addEdge("F", new HashSet<>(Arrays.asList("G", "H")));
-    DefaultCallGraph.addEdge("I", new HashSet<>(Arrays.asList("J")));
+    DefaultCallGraph.addEdge("A", Set.of("B", "C", "D"));
+    DefaultCallGraph.addEdge("D", Set.of("E", "F"));
+    DefaultCallGraph.addEdge("F", Set.of("G", "H"));
+    DefaultCallGraph.addEdge("I", Set.of("J"));
   }
 
   @Test
   void referencedClassMembers() {
-    HashSet<String> projectClasses = new HashSet<>(Arrays.asList("A"));
-    Set<String> referenced = DefaultCallGraph.referencedClassMembers(projectClasses);
+    Set<String> referenced = DefaultCallGraph.referencedClassMembers(Set.of("A"));
     // note that there is no path from A to J
-    Assertions.assertEquals(new HashSet<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H")), referenced);
+    Assertions.assertEquals(Set.of("A", "B", "C", "D", "E", "F", "G", "H"), referenced);
   }
 
   @Test
   void getProjectVertices() {
     Set<String> projectVertices = DefaultCallGraph.getProjectVertices();
-    Assertions.assertEquals(new HashSet<>(Arrays.asList("A", "D", "F", "I")), projectVertices);
+    Assertions.assertEquals(Set.of("A", "D", "F", "I"), projectVertices);
   }
 
   @Test
   void getUsagesPerClass() {
     Map<String, Set<String>> usagesPerClass = DefaultCallGraph.getUsagesPerClass();
     Map<String, Set<String>> usagesExpected = new HashMap<>();
-    usagesExpected.put("A", new HashSet<>(Arrays.asList("B", "C", "D")));
-    usagesExpected.put("D", new HashSet<>(Arrays.asList("E", "F")));
-    usagesExpected.put("F", new HashSet<>(Arrays.asList("G", "H")));
-    usagesExpected.put("I", new HashSet<>(Arrays.asList("J")));
+    usagesExpected.put("A", Set.of("B", "C", "D"));
+    usagesExpected.put("D", Set.of("E", "F"));
+    usagesExpected.put("F", Set.of("G", "H"));
+    usagesExpected.put("I", Set.of("J"));
     Assertions.assertEquals(usagesExpected, usagesPerClass);
   }
 
